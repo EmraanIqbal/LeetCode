24d4906bdb70d9cbb10a61e879c3dbeecd2b70ec
EmraanIqbal <se.imraniqbal@gmail.com>
2023-03-01 14:29:23 +0500

    merge sort

--- 912-SortAnArray.js
+++ 912-SortAnArray.js
@@ -14,18 +14,43 @@
 // Explanation: Note that the values of nums are not necessairly unique.
 
 // Bubble Sort
+// var sortArray = function (nums) {
+//   let sorted;
+//   do {
+//     sorted = false;
+//     for (let i = 0; i <= nums.length; i++) {
+//       if (nums[i] > nums[i + 1]) {
+//         let temp = nums[i + 1];
+//         nums[i + 1] = nums[i];
+//         nums[i] = temp;
+//         sorted = true;
+//       }
+//     }
+//   } while (sorted);
+//   return nums;
+// };
+
+// Merge Sort ALgorithm
 var sortArray = function (nums) {
-  let sorted;
-  do {
-    sorted = false;
-    for (let i = 0; i <= nums.length; i++) {
-      if (nums[i] > nums[i + 1]) {
-        let temp = nums[i + 1];
-        nums[i + 1] = nums[i];
-        nums[i] = temp;
-        sorted = true;
-      }
-    }
-  } while (sorted);
-  return nums;
+  if (nums.length < 2) return nums;
+  const mid = Math.floor(nums.length / 2);
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid);
+
+  return merge(sortArray(left), sortArray(right));
 };
+let merge = (left, right) => {
+  let sortedArray = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sortedArray.push(left.shift());
+    } else {
+      sortedArray.push(right.shift());
+    }
+  }
+  return [...sortedArray, ...left, ...right];
+};
+
+// let nums = [5,2,3,1];
+let nums = [5, 1, 1, 2, 0, 0];
+console.log(sortArray(nums));
